6693a7fbbfe50b141734c010dee5f176a713044e
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2023-03-26 20:39:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/smarthomejc/MainActivity.kt
@@ -0,0 +1,218 @@
+package com.example.smarthomejc
+
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.smarthomejc.ui.theme.SmartHomeJCTheme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SmartHomeJCTheme {
+                val navController = rememberNavController()
+                Scaffold(
+                    bottomBar = {
+                        BottomNavigationBar(
+                            items = listOf(
+                                BottomNavItem("Favorites", "favorites", Icons.Filled.Favorite),
+                                BottomNavItem("Things", "things", Icons.Filled.Search),
+                                BottomNavItem("Routines", "routines", Icons.Filled.Close),
+                                BottomNavItem("Ideas", "ideas", Icons.Filled.Info),
+                                BottomNavItem("Settings", "settings", Icons.Filled.Settings)
+                            ),
+                            navController = navController,
+                            onItemClick = {
+                                // Handle item click
+                                navController.navigate(it.route)
+                            }
+                        )
+                    },
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(
+                                    text = "My Smart Home",
+                                    textAlign = TextAlign.Center,
+                                    modifier = Modifier.fillMaxWidth(),
+                                    fontSize = 25.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            },
+                            backgroundColor = Color.Blue,
+                            elevation = 5.dp,
+                            contentColor = Color.White,
+                            actions = {
+                                IconButton(onClick = { /*TODO*/ }) {
+                                    Icon(
+                                        imageVector = Icons.Filled.Edit,
+                                        contentDescription = "Edit"
+                                    )
+                                }
+                            }
+                        )
+                    }
+
+                ) {
+                    Navigation(navController = navController)
+                }
+
+            }
+        }
+    }
+}
+
+
+@Composable
+fun BottomNavigationBar(
+    items: List<BottomNavItem>,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    onItemClick: (BottomNavItem) -> Unit
+) {
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    BottomNavigation(
+        modifier = modifier,
+        backgroundColor = Color.White,
+        elevation = 5.dp
+    ) {
+        items.forEach { item ->
+            val selected = backStackEntry.value?.destination?.route == item.route
+            BottomNavigationItem(
+                icon = {
+                    Column(horizontalAlignment = CenterHorizontally) {
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = item.name
+                        )
+                    }
+                },
+                label = { Text(item.name) },
+                selected = selected,
+                selectedContentColor = Color.Blue,
+                unselectedContentColor = Color.Black,
+                onClick = {
+                    onItemClick(item)
+                    navController.navigate(item.route)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun Navigation(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "favorites") {
+        composable("favorites") {
+            // Home screen
+            HomeScreen()
+        }
+        composable("things") {
+            // Things screen
+            ThingsScreen()
+        }
+        composable("routines") {
+            // Routines screen
+            RoutinesScreen()
+        }
+        composable("ideas") {
+            // Ideas screen
+            IdeasScreen()
+        }
+        composable("settings") {
+            // Settings screen
+            SettingsScreen()
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    Column {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Column {
+                Text(text = "Favorites")
+                FloatingActionButton(
+                    onClick = { /*TODO*/ },
+                    backgroundColor = Color.Blue,
+                    contentColor = Color.White,
+                    content = {
+                        Icon(
+                            imageVector = Icons.Filled.Add,
+                            contentDescription = "Add"
+                        )
+                    }
+                )
+            }
+        }
+
+    }
+}
+
+@Composable
+fun ThingsScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Things")
+    }
+}
+
+@Composable
+fun RoutinesScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Routines")
+    }
+}
+
+@Composable
+fun IdeasScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Ideas")
+    }
+}
+
+@Composable
+fun SettingsScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Settings")
+    }
+}
