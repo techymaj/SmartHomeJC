e863c21cee6b391b9e0dc854c3c7cb72e3665a01
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2023-05-08 17:13:56 +0300

    Moved RoutinesScreen UI to separate file

--- app/src/main/java/com/example/smarthomejc/RoutinesScreen.kt
+++ app/src/main/java/com/example/smarthomejc/RoutinesScreen.kt
@@ -16,7 +16,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
+// committed
 @Composable
 fun RoutinesScreen() {
     Box(
